7c5b38a4e5bf643d1f2f404ac38368125f3a9c52
Vince R <r2d2fan20@outlook.com>
2021-02-04 23:17:39 -0600

    A somewhat functioning app

---
+++ app/src/main/java/com/example/shadowbrowser/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.shadowbrowser
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        moreButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
