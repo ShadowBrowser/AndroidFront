d5a44ec2635aab4997840d266dc9614210856d51
Vince R <r2d2fan20@outlook.com>
2021-03-31 12:11:15 -0500

    Android is hard

--- app/src/main/java/com/example/shadowbrowser/MainActivity.kt
+++ app/src/main/java/com/example/shadowbrowser/MainActivity.kt
@@ -3,7 +3,13 @@ package com.example.shadowbrowser
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +17,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        moreButton.setOnClickListener {
+        /*moreButton.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
+        }*/
+        newtab_button.setOnClickListener {
+            Snackbar.make(it, "New Tab Created", Snackbar.LENGTH_SHORT).show()
         }
+    }
+
+    fun showMenu(v: View) {
+        /*PopupMenu(this, v).apply {
+            setOnMenuItemClickListener(this@MainActivity.onMenuItemClick())
+            inflate(R.menu.menu_main)
+            show()
+        }*/
+        val menu = PopupMenu(this, v)
+        menu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+            when(item.itemId) {
+                R.id.settings ->
+                    showSettings()
+                R.id.reload -> {
+                    Toast.makeText(this@MainActivity, "Reloading I Guess...", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        })
+        val inflater: MenuInflater = menu.menuInflater
+        inflater.inflate(R.menu.menu_main, menu.menu)
+        menu.show()
+    }
+
+    fun showSettings() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/shadowbrowser/SettingsActivity.kt
+++ app/src/main/java/com/example/shadowbrowser/SettingsActivity.kt
@@ -1,11 +1,32 @@
 package com.example.shadowbrowser
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        reset_shadow_btn.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(this)
+
+            //Set message
+            dialogBuilder.setMessage("Are you sure you want to reset Shadow Browser?")
+                    .setCancelable(true)
+                    .setPositiveButton("YES", DialogInterface.OnClickListener {
+                        dialog, id -> finish()
+                    })
+                    .setNegativeButton("NO", DialogInterface.OnClickListener {
+                        dialog, id -> dialog.cancel()
+                    })
+
+            val alert = dialogBuilder.create()
+            alert.setTitle("Confimation")
+            alert.show()
+
+        }
     }
 }
